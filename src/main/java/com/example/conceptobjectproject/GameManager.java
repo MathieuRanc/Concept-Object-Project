307de8647d3588ddc7ba64ddcb98e78bd0622e4d
nicolas <71765691+NicolasDebeurme@users.noreply.github.com>
2022-10-17 17:09:28 +0200

    Map update

--- src/main/java/com/example/conceptobjectproject/GameManager.java
+++ src/main/java/com/example/conceptobjectproject/GameManager.java
@@ -1,31 +1,46 @@
 package com.example.conceptobjectproject;
 
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GameManager extends Application {
 
-    private int mapWidth =16;
-    private int mapHeight = 8;
+    public GameManager Instance;
+
+    private Map _map;
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        Scene scene = new Scene(new Map(mapWidth,mapHeight).map, 1000, 562);
+        if(Instance !=null)
+            Instance=this;
+
+        _map = new Map(16,8,stage);
+        Scene gameWindow = new Scene(_map.GetMap(), 1000, 562);
+
+        SetupListener(gameWindow);
 
         stage.setTitle("SMA");
-        stage.setScene(scene);
+        stage.setScene(gameWindow);
         stage.show();
     }
 
+    private void SetupListener(Scene gameWindow)
+    {
+        gameWindow.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                _map.OnScreenResize(10,10);
+            }
+        });
+        gameWindow.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                System.out.println("Height: " + newSceneHeight);
+            }
+        });
+    }
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/com/example/conceptobjectproject/Map.java
+++ src/main/java/com/example/conceptobjectproject/Map.java
@@ -1,23 +1,19 @@
 package com.example.conceptobjectproject;
 
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class Map extends Parent {
 
-    public GridPane map;
+    private GridPane map;
 
-    public Map(int mapWidth, int mapHeight)
+    public Map(int mapWidth, int mapHeight, Stage stage)
     {
         map = new GridPane();
 
@@ -27,9 +23,18 @@ public class Map extends Parent {
 
         for (int i = 0; i < mapWidth; i++) {
             for (int j = 0; j < mapHeight; j++) {
-                map.add(new Rectangle( 50,50,(i+j) %2 ==0 ? Color.RED : Color.GREEN),i,j);
+                map.add(new Rectangle(stage.widthProperty().doubleValue(),stage.heightProperty().doubleValue(),(i+j) %2 ==0 ? Color.RED : Color.GREEN),i,j);
             }
         }
+
+    }
+
+    public GridPane GetMap()
+    {
+        return map;
+    }
+    public void OnScreenResize(float width, float height)
+    {
 
     }
     @Override
